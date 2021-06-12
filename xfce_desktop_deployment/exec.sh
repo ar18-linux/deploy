488af6ddc42354c6274f37b3275530d6a914be69
nulysses <adr.zivil@gmail.com>
2021-06-12 17:21:31 +0200

    1

--- xfce_desktop_deployment/exec.sh
+++ xfce_desktop_deployment/exec.sh
@@ -31,6 +31,7 @@ set -x
 
 . "${script_dir}/vars"
 echo "a18hf: ${ar18_helper_functions}"
+exit
 if [ -z "${ar18_helper_functions+x}" ]; then rm -rf "/tmp/helper_functions_$(whoami)"; cd /tmp; git clone https://github.com/ar18-linux/helper_functions.git; mv "/tmp/helper_functions" "/tmp/helper_functions_$(whoami)"; . "/tmp/helper_functions_$(whoami)/helper_functions/helper_functions.sh"; cd "${script_dir}"; export ar18_helper_functions=1; fi
 obtain_sudo_password
 
