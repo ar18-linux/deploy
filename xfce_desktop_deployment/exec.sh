20a1fc04c8bd38e820e85708f31afa489bdee984
nulysses <adr.zivil@gmail.com>
2021-06-12 19:41:47 +0200

    1

--- xfce_desktop_deployment/exec.sh
+++ xfce_desktop_deployment/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script template version 2021-06-12.01
+# Script template version 2021-06-12.02
 # Make sure some modification to LD_PRELOAD will not alter the result or outcome in any way
 LD_PRELOAD_old="${LD_PRELOAD}"
 LD_PRELOAD=
@@ -10,7 +10,7 @@ script_path="${script_dir}/$(basename "${0}")"
 #Set PS4 for easier debugging
 export PS4='\e[35m${BASH_SOURCE[0]}:${LINENO}: \e[39m'
 # Determine if this script was sourced or is the parent script
-if [ -z "${ar18_sourced_map+x}" ]; then
+if [ ! -v ar18_sourced_map ]; then
   declare -A -g ar18_sourced_map
 fi
 if [[ "${BASH_SOURCE[0]}" != "${0}" ]]; then
