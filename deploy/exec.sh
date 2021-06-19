e78ff94157f5d9bb02d2ade54569d3bf974acc39
nulysses <adr.zivil@gmail.com>
2021-06-19 15:30:57 +0200

    1

--- deploy/exec.sh
+++ deploy/exec.sh
@@ -37,7 +37,9 @@ if [ ! -v ar18_helper_functions ]; then rm -rf "/tmp/helper_functions_$(whoami)"
 obtain_sudo_password
 import_vars
 
-export ar18_deployment_target="${1}"
+set +u
+export ar18_deployment_target="$(read_target "${1}")"
+set -u
 
 source_or_execute_config "source" "deploy" "${ar18_deployment_target}"
 
